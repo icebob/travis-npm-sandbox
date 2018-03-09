2c17c027e2fd906a5b7ff44c16d2681293d581a1
Icebob <mereg.norbert@gmail.com>
2018-03-09 10:20:53 +0100

    add addition files

---
+++ build.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const pkg = require("./package.json");
+
+const fName = "./CHANGELOG.md";
+
+let content = fs.readFileSync(fName, "utf8");
+content += `Version: ${pkg.version} - ${new Date().toISOString()}\n`;
+
+fs.writeFileSync(fName, content, "utf8");
+
+console.log("Done!");
